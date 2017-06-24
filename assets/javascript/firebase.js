9d162c7849d1ef46da7d3e2d7044a86ea51f19fe
Papa Bravo Kilo <EchoChamber@Papas-MacBook-Pro.local>
2017-06-24 11:08:16 -0600

    this is a comment

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -1,15 +1,47 @@
 
-// function that sets 3 parameters at the start
-// 
+
+/**
+Root
+  Users
+    User1id
+    User2id
+    User3id
+      Name
+      Email
+      Bday
+      Recents
+        search 1
+        search 2
+        search 3
+        search 4
+        search 5      
+*/
 
 
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyDIjWxF-hQNpdWtmiPsaYtWLUp2KQCqSnc",
-    authDomain: "sandbox-b8347.firebaseapp.com",
-    databaseURL: "https://sandbox-b8347.firebaseio.com",
-    projectId: "sandbox-b8347",
-    storageBucket: "sandbox-b8347.appspot.com",
-    messagingSenderId: "1063986309166"
+    apiKey: "AIzaSyBgKBTuuGt03OVa2v5AIESL9IcvkZn5t5Q",
+    authDomain: "project-1-bbd03.firebaseapp.com",
+    databaseURL: "https://project-1-bbd03.firebaseio.com",
+    projectId: "project-1-bbd03",
+    storageBucket: "project-1-bbd03.appspot.com",
+    messagingSenderId: "581793472401"
   };
   firebase.initializeApp(config);
+
+  var database = firebase.database()
+
+  var userRef = database.ref("users")
+
+  // function that sets 3 parameters at the start: name, birthday and email (optional)
+  function setUserData(name, birthday, email){
+    var user = userRef.push()
+    user.set({
+      name: name, //key: value
+      birthday: birthday,
+      email: email,
+      recentSearches: null,
+    });
+  }
+  setUserData("brian", "10/17/1989","pbkingsley@gmail.com")
+
