53096431588d963d99857a3bfd02c5304fd3aeaa
Austin <elektricwebdesign@gmail.com>
2017-06-26 20:16:01 -0600

    Added new files and modified the html for the ids needed on the form input

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,6 +8,9 @@ function initAutocomplete() {
 $(document).ready(function() {
   const apiKey = 'AIzaSyCwOFrBMJrkRF4Q7MJ-ysYwEWKewhcJlyk';
   let initialMap;
+
+  // load our map script file once the dom is ready and our inital map has been called back with DATA
+  // this ensures we have access to the google object and start creating new maps
   (function(d, s, id){
     var js, mjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)){
@@ -24,6 +27,30 @@ $(document).ready(function() {
     mjs.parentNode.insertBefore(js, mjs);
   }(document, 'script', 'custom-maps-js'));
 
+
+  function searchUntap() {
+    let parsedParams = JSON.parse(localStorage.getItem('searchParams'));
+    let beerName = parsedParams.beerSearch || '';
+    let location = parsedParams.location || 'Colorado';
+    let breweryName = parsedParams.breweryName || '';
+    console.log(' WHAT IS THE PARSED AGAIN???', parsedParams);
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": `https://api.untappd.com/v4/search/beer?q=${beerName}+${location}&client_id=337761F7CE5C059F22A5D05E4182CD9AC5BF5711&client_secret=AAD5C7DC2CCD52B7A5E2721DA26765411A8F986B`,
+      "method": "GET",
+      "headers": {
+        "cache-control": "no-cache",
+        "postman-token": "5f4420b5-8852-c714-d77b-1aeb1858266f"
+      }
+    }
+
+$.ajax(settings).done(function (response) {
+  console.log(response, settings);
+});
+
+  }
+  searchUntap()
   //   (function() {
   //   $.ajax({
   //     url: `https://maps.GOOGLEapis.com/maps/api/place/nearbysearch/json?location=39.739236,-104.990251&radius=1609.34&type=restaurant&keyword=brewery&pagetoken&key=AIzaSyCwOFrBMJrkRF4Q7MJ-ysYwEWKewhcJlyk`,
