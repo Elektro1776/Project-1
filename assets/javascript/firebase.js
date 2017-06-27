a5a4056bcf31ac193f0b4e0360d29c6d70cf742b
Papa Bravo Kilo <EchoChamber@Papas-MacBook-Pro.local>
2017-06-26 19:00:18 -0600

    firebase update

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -1,3 +1,4 @@
+$(document).ready(function(){
 
 
 /**
@@ -27,21 +28,76 @@ Root
     storageBucket: "project-1-bbd03.appspot.com",
     messagingSenderId: "581793472401"
   };
+
   firebase.initializeApp(config);
+  
+  // firebase variables 
+  var database = firebase.database();
+  var userRef = database.ref("users");
 
-  var database = firebase.database()
+  // variable for today's date (to compare user's bday)
+  var today = new Date()
+  today = moment(today).format('YYYY-MM-DD');
 
-  var userRef = database.ref("users")
+
+
+console.log(today)
 
   // function that sets 3 parameters at the start: name, birthday and email (optional)
-  function setUserData(name, birthday, email){
+  function setUserData(userInputData){
+    console.log(userInputData)
     var user = userRef.push()
     user.set({
-      name: name, //key: value
-      birthday: birthday,
-      email: email,
+      name: userInputData.userName, //key: value
+      birthday: userInputData.buserBDay, //prop = field will need to reset
+      email: userInputData.userEmail,
       recentSearches: null,
     });
   }
-  setUserData("brian", "10/17/1989","pbkingsley@gmail.com")
+  // setUserData("Charlie", "07/04/1996","charlie@gmail.com")
+
+
+  // Capture user data on "Welcome to Beer Navigator" Take Off
+
+  //Stores form entries as an array and feeds array into userInput object
+  $('form').submit(function(e) {
+    e.preventDefault()
+    var userInput = {}
+    var $inputs = $('#FBcapture :input:not(:button)')
+    $inputs.each(function(index, value){
+      var prop = $(value)[0].id;
+      console.log(prop)
+      userInput[prop] = $(value).val()
+      console.log(index, value)
+
+    })
+
+ setUserData(userInput);
+
+
+
+
+   test = ((2017/06/26)-(1989/10/17))
+   console.log(test)
+
+  });
+
+    // if ((today - userBDay) < 21) {
+    //   // If true, take user to a new "Sorry Charlie" page
+    
+    // } else if(true) {
+    //     user.name.push(userName.val())
+    //     user.birthday.push(userBDay.val())
+    //     user.email.push(userEmail.val())
+    
+    // } else {
+
+    // };
+    
+});
+      
+
+
+
+
 
